649f950f36675d5036da79eb4d670460826d732d
Ander Liao <andercola@gmail.com>
2020-04-22 01:14:16 +0800

    Filter added

--- review_analysis.py
+++ review_analysis.py
@@ -18,8 +18,15 @@ print(len(data[0])) # Print out the length of 1st review
 print('-----------------')
 # Count the average length of each review
 sum_length = 0 # As a counter
-for d in data:
-	sum_length = sum_length + len(d) # Keep counting and add the length per review
+for review in data:
+	sum_length = sum_length + len(review) # Keep counting and add the length per review
 average_length = sum_length / len(data)
 print('The average length per review is', round(average_length))
-	
+print('-----------------')
+# Add filter => Create a list
+# Count the number of review that the length is less than 100
+less_100_filter = []
+for review in data:
+	if len(review) < 100:
+		less_100_filter.append(review)
+print('Number of review => Lengnth is less than 100:' ,len(less_100_filter))
